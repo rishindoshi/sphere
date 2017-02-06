cd46c6adedd8a21653c4a140bb1dc0f25d36407c
Rishin Doshi <rishdosh@umich.edu>
2017-02-06 12:59:56 -0500

    first commit. server init

---
+++ controllers/dummy_data.js
@@ -0,0 +1,1 @@
+// This module serves as a temp replacement for the AWS Database
---
+++ controllers/genres.js
@@ -0,0 +1,10 @@
+// This module is responsible for all functions that deal with music genres
+
+exports.buildColorTree = function() {
+  // First need to import list of genres in JSON form from external file
+  // Where should we keep this file?
+
+
+
+  return {};
+}
