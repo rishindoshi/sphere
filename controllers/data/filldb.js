7dd73ed3057d661f7a1a65125a158e6f941a0baa
Rishin Doshi <rishdosh@umich.edu>
2017-03-20 17:25:13 -0400

    more filldb edits

--- controllers/data/filldb.js
+++ controllers/data/filldb.js
@@ -7,10 +7,38 @@ var MongoClient = require('mongodb').MongoClient;
 var genClient = require('./gendata');
 var mongoUrl = 'mongodb://rishdosh:Moniter123@ds131320.mlab.com:31320/sphere';
 
+MongoClient.connect(mongoUrl, function (err, db) {
+  if (err) return console.log(err);
+  else {
+    dbFill(db);
+  }
+});
 
-// MongoClient.connect(mongoUrl, function (err, db) {
-//   if (err) return console.log(err);
-//   else {
-//
-//   }
-// });
+var dbFill = function(db) {
+  clearAllCollections(db)
+    .then(function(status) {
+      console.log(status);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+var clearAllCollections = function(db) {
+  var deferred = Q.defer();
+  db.collection("users").deleteMany({'type': 'explorer'}, function(err, r) {
+    if (err) deferred.reject(err);
+    else {
+      db.collection("users").deleteMany({'type': 'vendor'}, function(err, r) {
+        if (err) deferred.reject(err);
+        else {
+          db.collection("venues").remove({}, function(err, r){
+            if (err) deferred.reject(err);
+            else deferred.resolve("success: clearAllCollections");
+          })
+        }
+      });
+    }
+  });
+  return deferred.promise;
+}
