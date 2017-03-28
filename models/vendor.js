d950933d649ebe28a8ecb0ee9f223eb52b96a9a1
Rishin Doshi <rishdosh@umich.edu>
2017-03-28 18:49:23 -0400

    changed vendor schema back to required: true

--- models/vendor.js
+++ models/vendor.js
@@ -11,7 +11,7 @@ var vendorSchema = new Schema({
 	venueName: String, 
 	lat: { type: Number, required: true }, 
 	lng: { type: Number, required: true }, 
-	address: { type: String }
+	address: { type: String, required: true }
 });
 
 var Vendor = mongoose.model('Vendor', vendorSchema);
